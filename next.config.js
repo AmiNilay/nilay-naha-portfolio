17f42d00a2c2b1a94e0df4f7568067dd0af12b50
AmiNilay <niloynaha2003@.com>
2026-08-18 00:10:54 +0530

    Whitelist GitHub image domains

--- next.config.js
+++ next.config.js
@@ -1,25 +1,17 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
+const withPWA = require("next-pwa")({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  // Disable PWA in development so it doesn't aggressively cache while you are coding
   disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**", // Allows images from GitHub, etc.
-      },
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "github.com" }
     ],
   },
 };
